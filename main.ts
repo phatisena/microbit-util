66bade1b87cef5cf30d43ad9d43a78109a574fc9
Phatisena <naphatism@hotmail.com>
2024-12-28 11:51:30 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,68 @@
+//%block="UtilBit"
+//%color="#f2b72e"
+//%icon="\uf0e7"
+namespace util {
+
+    export enum nullType { Undefined = 1, Null = 2, Nan = 3}
+
+    //%blockid=util_nulltype
+    //%block="$t"
+    //%group="null value"
+    //%weight=1
+    export function getNull(t:nullType): any {
+        switch (t) {
+            case 1:
+            return undefined
+            case 2:
+            return null
+            case 3:
+            return NaN
+            default:
+            return undefined
+        }
+    }
+
+    let mathPi = 3.14159265359
+    let mathPhi = 1.6180339887
+
+    export enum mathTval {
+        //%block="π"
+        Pi = 1,
+        //%block="φ"
+        Phi = 2
+    }
+
+    //%blockid=util_mathtemp
+    //%block="$t"
+    //%group="math temp"
+    //%weight=1
+    export function mathTmp(t:mathTval) {
+        switch (t) {
+            case 1:
+            return mathPi;
+            case 2:
+            return mathPhi;
+            default:
+            return -1
+        }
+    }
+
+    export enum runType { inBackground = 1, untilDone = 2}
+
+    //%blockid=util_runtype
+    //%block="on run in zone $t do"
+    //%group="timer"
+    //%weight=1
+    export function runInDo(t:runType, thendo:() => void ) {
+        switch (t) {
+            case 1:
+            control.runInBackground(thendo)
+            return;
+            case 2:
+            control.runInParallel(thendo)
+            return;
+            default:
+            return;
+        }
+    }
+}
